59a1583f79ecd74b8700ebae389e4027b3185626
jeferson-gallo <jdgap9801@gmail.com>
2021-04-12 11:28:27 -0500

    Warnings fixed

--- app/src/main/java/com/jandres/currencytransform/CurrencyArrayAdapter.kt
+++ app/src/main/java/com/jandres/currencytransform/CurrencyArrayAdapter.kt
@@ -11,23 +11,23 @@ import android.widget.TextView
 
 class CurrencyArrayAdapter(context: Context,currencyList: List<Currencys> ) : ArrayAdapter<Currencys>(context, 0,currencyList){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return initView(position,convertView,parent) }
+        return initView(position, parent) }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return initView(position,convertView,parent) }
+        return initView(position, parent) }
 
-    private fun initView(position: Int, convertView: View?, parent: ViewGroup) : View {
+    private fun initView(position: Int, parent: ViewGroup) : View {
 
         val currency = getItem(position)
 
         val view = LayoutInflater.from(context).inflate(R.layout.spinner_item, parent,false)
 
-        var image : ImageView = view.findViewById<ImageView>(R.id.currencyImageView)
-        var text : TextView = view.findViewById<TextView>(R.id.currencyNameTextView)
+        val image : ImageView = view.findViewById(R.id.currencyImageView)
+        val text : TextView = view.findViewById(R.id.currencyNameTextView)
 
 
         image.setImageResource(currency!!.image)
-        text.setText(currency.name)
+        text.text = currency.name
 
         return view }
 
